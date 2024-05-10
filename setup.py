31dcbf46b6fb0cd89f4e15c8fc4fe953e3ad175c
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2024-05-10 14:55:58 +0530

    readme changes and packaging for pypi initial push

--- setup.py
+++ setup.py
@@ -2,25 +2,39 @@ from setuptools import setup, find_packages
 
 setup(
     name="prodmatic",
-    version="2024.04.29",
-    description="A Python package to create in app products and subscriptions on playstore and appstore",
+    version="2024.05.10",
+    description="A Python package for seamless management and pricing of in-app products and subscriptions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Gopala Krishna Koduri",
     author_email="gopal@riyazapp.com",
-    url="https://riyazapp.com",
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        "": ["resources/*.csv", "resources/*.json"],
+    url="https://github.com/musicmuni/prodmatic",
+    project_urls={
+        "Source Code": "https://github.com/musicmuni/prodmatic",
+        "Issue Tracker": "https://github.com/musicmuni/prodmatic/issues",
+        "Connect w/ Author": "https://linkedin.com/in/gopalkoduri",
+        "Riyaz - Learn to sing": "https://riyazapp.com",
     },
+    packages=find_packages(),
+    package_data={
+        "prodmatic": [
+            "resources/*.csv",
+            "resources/*.json",
+        ],
+    },
+    include_package_data=True,
     install_requires=[
-        # List your package dependencies here
-        # 'numpy', 'pandas', etc.
+        "requests",
+        "beautifulsoup4",
+        "pyrestcountries",
+        "pppfy",
+        "moneymatters",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
